21d7119715e9cbaeda68be61fcd89b94b705278b
Kaks <kmahtaj57@gmail.com>
2025-03-25 11:04:47 +0100

    Ajout des fichiers dans le répertoire public

--- src/data/cars.ts
+++ src/data/cars.ts
@@ -30,20 +30,10 @@ export const cars: Car[] = [
     image: "/WhatsApp Image 2025-03-24 at 13.04.28.jpeg",
     features: ["V10 5.2L", "640 CV", "0-100 km/h en 2.8s", "Transmission 7 vitesses"]
   },
-  {
-    id: 3,
-    name: "Bentley Continental GT",
-    brand: "Bentley",
-    price: "210 000€",
-    description:
-      "Le luxe britannique par excellence. Alliance parfaite entre confort, technologie et performances.",
-    image:
-      "https://images.unsplash.com/photo-1580273916550-e323be2ae537?fit=crop&w=1920&q=80",
-    features: ["W12 6.0L", "635 CV", "0-100 km/h en 3.6s", "Intérieur cuir premium"],
-  },
+
   
   {
-    id: 2,
+    id: 3,
     name: "A180 AMG line",
     brand: "Mercedes",
     price: "25 490€",
@@ -52,7 +42,7 @@ export const cars: Car[] = [
     features: ["V10 5.2L", "640 CV", "0-100 km/h en 2.8s", "Transmission 7 vitesses"]
   },
   {
-    id: 3,
+    id: 4,
     name: "A35 AMG",
     brand: "Mercedes",
     price: "37 350€",
@@ -62,7 +52,7 @@ export const cars: Car[] = [
   },
 
   {
-    id: 4,
+    id: 5,
     name: "A35 AMG",
     brand: "Mercedes",
     price: "39 500€",
@@ -71,7 +61,7 @@ export const cars: Car[] = [
     features: ["W12 6.0L", "635 CV", "0-100 km/h en 3.6s", "Intérieur cuir premium"]
   },
   {
-    id: 5,
+    id: 6,
     name: "812gtie",
     brand: "Ferrari",
     price: "359 O00€",
@@ -80,7 +70,7 @@ export const cars: Car[] = [
     features: ["W12 6.0L", "635 CV", "0-100 km/h en 3.6s", "Intérieur cuir premium"]
   },
   {
-    id: 6,
+    id: 7,
     name: "296gtb",
     brand: "Ferrari",
     price: "370 O00€",
@@ -90,7 +80,7 @@ export const cars: Car[] = [
   },
 
   {
-    id: 7,
+    id: 8,
     name: "huracan evo lp 640",
     brand: "Lamborghini",
     price: "238 900€",
@@ -100,7 +90,7 @@ export const cars: Car[] = [
   },
 
   {
-    id: 8,
+    id: 9,
     name: "CLE 220 coupe amg line 9g tronic ",
     brand: "Mercedes",
     price: "63 990€",
@@ -110,7 +100,7 @@ export const cars: Car[] = [
   },
 
   {
-    id: 9,
+    id: 10,
     name: "Urus 4.0 ",
     brand: "Lamborghini ",
     price: "228 900€",
